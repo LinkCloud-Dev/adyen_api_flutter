052cd21f110ec1f9e4473d859198a90a50a2e6c7
zwan8710 <zwan8710@uni.sydney.edu.au>
2024-10-21 17:58:39 +1100

    set up adyen api library and gradle build files

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <adyen_api_flutter/adyen_api_flutter_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AdyenApiFlutterPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AdyenApiFlutterPluginCApi"));
+}
